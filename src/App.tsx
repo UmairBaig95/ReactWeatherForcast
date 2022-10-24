5dc99db520d042b1da0b45981b29013693c9e66b
Ankit <ankit@boo.world>
2022-10-24 14:11:27 +0500

    added component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.less';
+import WeatherComponent from './components/WeatherComponent' 
 
 class App extends React.Component {
 render() {
   return (
-    <div>asd</div>
+    <WeatherComponent />
   )
 }
 }
---
+++ src/components/WeatherComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class WeatherComponent extends React.Component {
+    render() {
+        return <div>Weather COmponent</div>
+    }
+}
+export default WeatherComponent;
