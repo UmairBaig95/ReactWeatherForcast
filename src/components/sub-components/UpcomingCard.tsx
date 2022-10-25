f50c7cb4a6a34baae36bf0c86ba18826af5fa7c6
Talha <baigtalha7@gmail.com>
2022-10-25 11:14:45 +0500

    update

--- src/components/sub-components/UpcomingCard.tsx
+++ src/components/sub-components/UpcomingCard.tsx
@@ -1,33 +1,43 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {fas} from "@fortawesome/free-solid-svg-icons"
-library.add(fas)
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
 
-export default class UpcomingCard extends React.Component<{}, { dataweather: any}>  {
-  constructor(props:any){
-    super(props); 
-    this.state={
-      dataweather : props
-    }
-
+export default class UpcomingCard extends React.Component<
+  {},
+  { dataweather: any }
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      dataweather: props,
+    };
   }
   render() {
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const weekday = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
     const d = new Date();
     const model = this.state.dataweather;
-    var tifOptions : any = [];
-    Object.keys(model).forEach(function(key) {
-    tifOptions.push(model[key]);
-}); 
-console.log(tifOptions[1]);
+    var tifOptions: any = [];
+    Object.keys(model).forEach(function (key) {
+      tifOptions.push(model[key]);
+    });
+    console.log(tifOptions);
 
-    return <div className="upcomingContainer flex">
-     {tifOptions.map((e:any)=> {return (  <div className="upcoming-card"><h3>{}</h3>
-      <FontAwesomeIcon icon={["fas", "cloud"]} />
-      <span>19&#176;</span>
-      </div>)})}
-      {/* <div className="upcoming-card"><h3>Wed</h3>
+    return (
+      <div className="upcomingContainer flex">
+        {tifOptions.map((e: any) => {
+          const date = weekday[new Date(e.dt_txt).getDay()];
+          return (
+            <div className="upcoming-card">
+              <h3>{date.toString()}</h3>
+              <img src={`http://openweathermap.org/img/w/${e.weather[0].icon}.png`} alt="" />
+              
+              <span>{e.main.temp}&#176;</span>
+            </div>
+          );
+        })}
+        {/* <div className="upcoming-card"><h3>Wed</h3>
       <FontAwesomeIcon icon={["fas", "cloud"]} />
       <span>19&#176;</span>
       </div>
@@ -43,6 +53,7 @@ console.log(tifOptions[1]);
       <FontAwesomeIcon icon={["fas", "cloud"]} />
       <span>19&#176;</span>
       </div> */}
-    </div>
+      </div>
+    );
   }
 }
